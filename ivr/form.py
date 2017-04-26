de7b9ae73d69e60471c68ac17ac70e5a72f2ef1d
Francois Blackburn <fblackburn@wazo.community>
2017-04-26 11:58:19 -0400

    add validators on fields

--- ivr/form.py
+++ ivr/form.py
@@ -4,35 +4,35 @@
 
 from wtforms.fields import (SubmitField,
                             StringField,
-                            IntegerField,
                             FormField,
                             FieldList,
                             SelectField)
+from wtforms.fields.html5 import IntegerField
 from wtforms.validators import (InputRequired,
-                                DataRequired,
-                                Optional)
+                                Length,
+                                NumberRange)
 
 from wazo_admin_ui.helpers.destination import DestinationHiddenField, DestinationField
 from wazo_admin_ui.helpers.form import BaseForm
 
 
 class IvrChoiceForm(BaseForm):
-    exten = StringField()
+    exten = StringField(validators=[InputRequired(), Length(max=40)])
     destination = DestinationField(destination_label='')
 
 
 class IvrForm(BaseForm):
-    name = StringField('Name', [InputRequired()])
+    name = StringField('Name', [InputRequired(), Length(max=128)])
     abort_destination = DestinationField(destination_label='Abort destination')
-    abort_sound = StringField('Abort sound')
+    abort_sound = StringField('Abort sound', [Length(max=255)])
     choices = FieldList(FormField(IvrChoiceForm))
     description = StringField('Description')
-    greeting_sound = StringField('Greeting sound')
+    greeting_sound = StringField('Greeting sound', [Length(max=255)])
     invalid_destination = DestinationField(destination_label='Invalid destination')
-    invalid_sound = StringField('Invalid sound')
-    max_tries = IntegerField('Max tries', default=3, validators=[Optional()])
-    menu_sound = StringField('Menu sound', validators=[DataRequired()])
-    timeout = IntegerField('Timeout', default=0, validators=[Optional()])
+    invalid_sound = StringField('Invalid sound', [Length(max=255)])
+    max_tries = IntegerField('Max tries', default=3, validators=[NumberRange(min=1)])
+    menu_sound = StringField('Menu sound', validators=[InputRequired(), Length(max=255)])
+    timeout = IntegerField('Timeout', default=0, validators=[NumberRange(min=0)])
     timeout_destination = DestinationField(destination_label='Timeout destination')
     submit = SubmitField('Submit')
 
@@ -40,5 +40,5 @@ class IvrForm(BaseForm):
 class IvrDestinationForm(BaseForm):
     setted_value_template = u'{ivr_name}'
 
-    ivr_id = SelectField('IVR', choices=[])
+    ivr_id = SelectField('IVR', [InputRequired()], choices=[])
     ivr_name = DestinationHiddenField()
